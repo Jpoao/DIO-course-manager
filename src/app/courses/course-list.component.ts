a0a6d58dc40b8b31b84730fd72544a82119fd406
João Paulo <mg_ses@hotmail.com>
2021-08-04 16:19:56 -0300

    courses dependecy injection

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
+import { CourseService } from "./course.service";
 
 @Component({
   selector: "app-course-list",
@@ -9,31 +10,11 @@ export class CourseListComponent implements OnInit {
 
   courses: Course[] = [];
 
+  constructor(private courserService: CourseService) { }
+
   ngOnInit(): void {
-    this.courses = [
-      {
-        id: 1,
-        name: "Angular: Forms",
-        imgUrl: "/assets/images/forms.png",
-        price: 99.99,
-        code: "XPS-8796",
-        duration: 120,
-        rating: 5,
-        releaseDate: "December, 2, 2019",
-        options: ""
-      },
-      {
-        id: 2,
-        name: "Angular: HTTP",
-        imgUrl:  "/assets/images/http.png",
-        price: 45.99,
-        code: "LKL-1094",
-        duration: 80,
-        rating: 4,
-        releaseDate: "December, 4, 2019",
-        options: ""
-      }
-    ]
+    this.courses = this.courserService.retriveAll();
+     
   }
 
 
