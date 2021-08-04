edadb166e07f1a1711d955fa6e2e0a0feddb8495
João Paulo <mg_ses@hotmail.com>
2021-08-04 19:31:34 -0300

    Nav bar and basic routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,39 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from './stars/star-component';
+import { ReplacePipe } from './pipes/replace.pipe';
+import { NavBarComponent } from 'src/core/nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
+import { Error404 } from 'src/core/error404/error404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
-    StarComponent
+    StarComponent,
+    ReplacePipe,
+    NavBarComponent,
+    Error404
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: "", 
+        redirectTo: "courses", 
+        pathMatch: "full"
+      },
+      {
+        path: "courses",
+        component: CourseListComponent,
+        pathMatch: "full"
+      },
+      {
+        path: "**",
+        component: Error404
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
